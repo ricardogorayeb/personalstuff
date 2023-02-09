4d005cb2256a3f8e9f28d75dee1d044f867b47ca
ricardogorayeb <ricardogorayeb@gmail.com>
2023-02-09 11:17:56 -0400

    Script for test bandwidth

--- test_bandwidth.sh
+++ test_bandwidth.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 ZABBIXSENDER=`which zabbix_sender`
 
+PING=`ssh muran@172.25.40.35 "/home/muran/speedtest-cli --simple | cut -d' ' -f2 | sed -n 1p"`
 
 
-PING=`/home/muran/speedtest-cli --simple | cut -d' ' -f2 | sed -n 1p`
-DOWNLOAD=`/home/muran/speedtest-cli --simple | cut -d' ' -f2 | sed -n 2p`
-UPLOAD=`/home/muran/speedtest-cli --simple | cut -d' ' -f2 | sed -n 3p`
-
-
-$ZABBIXSENDER  -z 172.20.5.144 -s "lwrs-sbtt" -k ping_value -o `echo $PING`
-$ZABBIXSENDER  -z 172.20.5.144 -s "lwrs-sbtt" -k download_value -o `echo $DOWNLOAD`
-$ZABBIXSENDER  -z 172.20.5.144 -s "lwrs-sbtt" -k upload_value -o `echo $UPLOAD`
+if [ $? -eq 0 ]
+then
+	DOWNLOAD=`ssh muran@172.25.40.35 "/home/muran/speedtest-cli --simple | cut -d' ' -f2 | sed -n 2p"`
+	UPLOAD=`ssh muran@172.25.40.35 "/home/muran/speedtest-cli --simple | cut -d' ' -f2 | sed -n 3p"`
+    $ZABBIXSENDER  -z 172.20.5.144 -s "lwrs-sbtt" -k ping_value -o `echo $PING`
+	$ZABBIXSENDER  -z 172.20.5.144 -s "lwrs-sbtt" -k download_value -o `echo $DOWNLOAD`
+	$ZABBIXSENDER  -z 172.20.5.144 -s "lwrs-sbtt" -k upload_value -o `echo $UPLOAD`
+else
+	$ZABBIXSENDER  -z 172.20.5.144 -s "lwrs-sbtt" -k ping_value -o 0
+	$ZABBIXSENDER  -z 172.20.5.144 -s "lwrs-sbtt" -k download_value -o 0
+	$ZABBIXSENDER  -z 172.20.5.144 -s "lwrs-sbtt" -k upload_value -o 0
+fi
