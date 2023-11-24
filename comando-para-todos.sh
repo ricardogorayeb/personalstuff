66f9345cb31d8532b254203c89e93abc7cb0d006
ricardogorayeb <ricardogorayeb@gmail.com>
2023-11-24 09:58:27 -0400

    Add new issues on comando-para-todos.sh

--- comando-para-todos.sh
+++ comando-para-todos.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-
-#for n in $(seq 1 9); do 
-#  echo "Executando comando \"$1\" em blade0${n}" 
-#  ssh  -o "StrictHostKeyChecking no" blade0${n} $1
-#done
-for n in $(seq 11 16); do 
-  echo "Executando comando \"$1\" em blade${n}" 
-  ssh  -o "StrictHostKeyChecking no" blade${n} $1
+#########################
+# Usage: bash comando-para-todos.sh <user> <command>
+########################
+for n in $(seq 2 9); do 
+  echo "Executando comando \"$2\" como \"$1\" em srvcrmncluster${n}p" 
+  ssh  -o "StrictHostKeyChecking no" $1@srvcrmncluster${n}p $2
+done
+for n in $(seq 10 12); do 
+  echo "Executando comando \"$2\" como \"$1\" em srvcrmncluster${n}p" 
+  ssh  -o "StrictHostKeyChecking no" $1@srvcrmncluster${n}p $2
 done
 #for n in $(seq 1 2); do 
 #  echo "Executando comando \"$1\" em r710${n}" 
