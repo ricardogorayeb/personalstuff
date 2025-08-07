ba847efda722e32a06c550c91786d40388164c08
Ricardo Gorayeb <67667493+ricardogorayeb@users.noreply.github.com>
2025-08-07 13:39:38 -0400

    Update and rename kill_older_processes to kill_older_processes.sh
    Rename file and add source information.

--- kill_older_processes.sh
+++ kill_older_processes.sh
@@ -1,4 +1,6 @@
 #List and kill all USER processes older than TIME
+#Source:https://www.baeldung.com/linux/kill-all-processes-older-than-age
+
 TIME=9600 
 USER=pid_user
 ps -eo pid,etimes,cmd,user | awk '$2 > $TIME && $5 == "$USER"' | awk '{ print $1 }' | xargs kill -9
