5214ba293f958392deadf2dd9440227a89f81809
Ricardo Gorayeb <67667493+ricardogorayeb@users.noreply.github.com>
2025-08-19 13:33:55 -0400

    Update kill_older_processes.sh

--- kill_older_processes.sh
+++ kill_older_processes.sh
@@ -2,5 +2,5 @@
 #Source:https://www.baeldung.com/linux/kill-all-processes-older-than-age
 
 TIME=9600 
-USER=pid_user
+USER=user
 ps -eo pid,etimes,cmd,user | awk '$2 > $TIME && $5 == "$USER"' | awk '{ print $1 }' | xargs kill -9
