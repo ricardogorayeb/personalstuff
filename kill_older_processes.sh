6ed28661d826b01a80b30f75340da84dd918a0ef
Ricardo Gorayeb <67667493+ricardogorayeb@users.noreply.github.com>
2025-09-16 14:22:39 -0400

    Update kill_older_processes.sh

--- kill_older_processes.sh
+++ kill_older_processes.sh
@@ -3,5 +3,5 @@
 #Source: Google IA
 
 TIME=9600 
-ps -eo pid,etimes,cmd,user | awk -v user=<your_user> '{if ($2>="$TIME" && $4==user)  print $1}' | grep -v grep
+ps -eo pid,etimes,cmd,user | awk -v user=<your_user> '{if ($2>="$TIME" && $4==user)  print $1}' | xargs kill -9
 
