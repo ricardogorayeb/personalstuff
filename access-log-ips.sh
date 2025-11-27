ca07b3fc1f819317d34c5e411fd10f7df688c795
Ricardo Gorayeb <ricardo.gorayeb@CRMN6370729L.rede.sipam.gov.br>
2025-11-27 14:32:34 -0400

    lot of changes

--- access-log-ips.sh
+++ access-log-ips.sh
@@ -2,6 +2,4 @@
 ## and send it to Zabbix Server
 
 
-
-
 zabbix_sender -z <zabbix_ip_address> -s "<host_on_zabbix>" -k <item_key> -o "`cat /var/log/apache2/access.log | awk '{ print $1 }' | sed 's/,//' | sort | uniq -c 
