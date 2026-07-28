77d6d6297944cebaae6706981a2e4a644e513696
Ricardo Gorayeb <ricardogorayeb@gmail.com>
2026-07-28 15:51:06 -0400

    feat: add command to list and sort top IP addresses from access log

--- access-log-ips.sh
+++ access-log-ips.sh
@@ -3,3 +3,8 @@
 
 
 zabbix_sender -z <zabbix_ip_address> -s "<host_on_zabbix>" -k <item_key> -o "`cat /var/log/apache2/access.log | awk '{ print $1 }' | sed 's/,//' | sort | uniq -c 
+
+
+### IPs que mais acessaram
+
+awk '{print $1}' access.log | sort | uniq -c | sort -nr | head -n 1
