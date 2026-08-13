816f28dc989c80238b3b36bb1686f83112b59c6d
Ricardo Gorayeb <ricardogorayeb@gmail.com>
2026-08-13 11:25:53 -0400

    checkVol update

--- checkVol.sh
+++ checkVol.sh
@@ -1,36 +1,11 @@
 #!/bin/bash
+exec >> /home/muran/Scripts_FTP_Radar/saida_script.log 2>&1
 
-#Verifica se o script está executando a mais de 7200segundos e 
-# mata o processo caso seja verdade
-
-ps -eo pid,etimes,cmd | awk '$2 > 7200 && $3 == "/home/muran/Scripts_FTP_Radar/checkVol.sh"' | xargs kill -9
-
-# Controle de execuções simultâneas
-# Evita que haja mais de uma instância deste script executando.
-
-ESTE=$(basename ${0})
-LOCKFILE=/tmp/${ESTE}.pid
-
-if [[ -s ${LOCKFILE} ]]; then
-	PIDLOCK=$(cat ${LOCKFILE})
-	if ! kill -0 ${PIDLOCK} 2> /dev/null; then
-		echo "${LOCKFILE} existe, mas o processo ${PIDLOCK} não está em execução. Continuando.";
-	else
-		echo "Script ${ESTE} ainda está em execução (PID ${PIDLOCK}). Saindo."
-		exit 2;
-	fi
-fi
-
-echo $$ > "${LOCKFILE}";
-
-BASENAME=`which basename`
-SERVICE=rsync
-RSYNC=`which rsync`
-BZIP2=`which bzip2`
-BASENAME=`which basename`
-ZABBIXSENDER=`which zabbix_sender`
-INVALIDO=0
-NOME_MAQUINA=`hostname`
+BASENAME=$(which basename)
+RSYNC=$(which rsync)
+BZIP2=$(which bzip2)
+ZABBIXSENDER=$(which zabbix_sender)
+NOME_MAQUINA=$(hostname)
 LOCAL_RADAR=${NOME_MAQUINA: -4}
 
 DIRVMET="/home/muran/data/rdc-$LOCAL_RADAR/scan_vmet/"
@@ -39,82 +14,111 @@ DIRXPPI="/home/muran/data/rdc-$LOCAL_RADAR/scan_xppi/"
 DIRBASEVMET="/home/muran/MVOL.sinc/scan_vmet"
 DIRBASEXPPI="/home/muran/MVOL.sinc/scan_xppi"
 
-DIRRSYNCVMET=rsync://root@radproc:/Volumes${LOCAL_RADAR^^}_VMET
-DIRRSYNCXPPI=rsync://root@radproc:/Volumes${LOCAL_RADAR^^}_XPPI
+DIRRSYNCVMET="rsync://root@radproc:/Volumes${LOCAL_RADAR^^}_VMET"
+DIRRSYNCXPPI="rsync://root@radproc:/Volumes${LOCAL_RADAR^^}_XPPI"
 
-HORAMODDIR=$(( `stat -c %Y $DIRVMET` ))
-HORATUAL=$(( `date +%s` ))
-TEMPODIF=$(( $HORATUAL - $HORAMODDIR ))
+DIRVMET_VALIDOS="$DIRVMET/0movidos"
+DIRVMET_INVALIDOS="$DIRVMET/0invalidos"
 
-# arquivos de controle para RSYNC
+DIRXPPI_VALIDOS="$DIRXPPI/0movidos"
+DIRXPPI_INVALIDOS="$DIRXPPI/0invalidos"
+# Define your directories in an array
+dirs=("$DIRBASEVMET" "$DIRBASEXPPI" "$DIRVMET_VALIDOS" "$DIRVMET_INVALIDOS" "$DIRXPPI_VALIDOS" "$DIRXPPI_INVALIDOS")
 
-#SCAN VMET
-echo "CHECANDO SCAN VMET"
-
-for ARQFULL in $(ls $DIRVMET/*.mvol | tail -n850);do
-	ARQ=`basename $ARQFULL`
-	NUMVOL=$(strings $DIRVMET/$ARQ | grep scan[0-9]|wc -l)
-	echo "QTD ARQS:"$NUMVOL
-	if [ $NUMVOL -eq 17 ]; then
-		INVALIDO=0
-		NOMEVOL=$( $BASENAME $ARQ | sed -r 's/(.*)\--(.{2})\:(.{2})\:(.*\..*)/\1\_\2\3/')
-		ANO=$(echo ${NOMEVOL} | cut -c1,2,3,4)
-		MES=$(echo ${NOMEVOL} | cut -c6,7)
-		DIA=$(echo ${NOMEVOL} | cut -c9,10)
-		if [ -s $DIRBASEVMET/$ANO/$MES/$DIA/${NOMEVOL}.mvol.bz2 ]; then
-			echo "ARQUIVO EXISTE" $NOMEVOL
-			#mv $DIRVMET/$ARQ $DIRVMET/0movidos/$ARQ
-		else	
-			echo "ORGANIZANDO.. ARQUIVO VMET " $ARQ
-			if [ ! -d $DIRBASEVMET/$ANO ]; then mkdir $DIRBASEVMET/$ANO; fi
-			if [ ! -d $DIRBASEVMET/$ANO/$MES ]; then mkdir $DIRBASEVMET/$ANO/$MES; fi
-			if [ ! -d $DIRBASEVMET/$ANO/$MES/$DIA ]; then mkdir $DIRBASEVMET/$ANO/$MES/$DIA; fi
-			echo "COPIANDO ARQUIVO"  $ARQ
-			$(cp -rp $DIRVMET/$ARQ $DIRBASEVMET/$ANO/$MES/$DIA/${NOMEVOL}.mvol)
-			mv $DIRVMET/$ARQ $DIRVMET/0movidos/$ARQ
-			bzip2 -f $DIRBASEVMET/$ANO/$MES/$DIA/${NOMEVOL}.mvol
-		fi
-	else
-		echo $ARQ
-		echo "entrou aqui ... ${NUMVOL}"
-		INVALIDO=1
-	fi
+for dir in "${dirs[@]}"; do
+    if [ ! -d "$dir" ]; then
+    	mkdir -p $dir
+    fi
 done
-   
-rsync -avzr --no-t --remove-source-files --exclude-from=/home/muran/exclude.txt --files-from=<(ls $DIRBASEVMET/ | tail -n850) $DIRBASEVMET/ $DIRRSYNCVMET 
 
-#SCAN XPPI
-echo "CHECANDO  SCAN XPPI"
-for ARQ in $(ls $DIRXPPI | tail -n350);do
-	NUMVOL=$(strings $DIRXPPI/$ARQ | grep scan[0-9]|wc -l)
-	echo "$NUMVOL"
 
-        if [ $NUMVOL -eq 3 ]; then
-		NOMEVOL=$( $BASENAME $ARQ | sed -r 's/(.*)\--(.{2})\:(.{2})\:(.*\..*)/\1\_\2\3/')
-		ANO=$(echo ${NOMEVOL} | cut -c1,2,3,4)
-		MES=$(echo ${NOMEVOL} | cut -c6,7)
-		DIA=$(echo ${NOMEVOL} | cut -c9,10)
+while true; do
+	INVALIDO=0
 
-		if [ -s $DIRBASEXPPI/$ANO/$MES/$DIA/${NOMEVOL}_400.mvol.bz2 ]; then
-			echo "ARQUIVO EXISTE" $NOMEVOL
-			mv $DIRXPPI/$ARQ $DIRXPPI/0movidos/$ARQ
+	# SCAN VMET
+	echo "CHECANDO SCAN VMET"
+	for ARQFULL in $(ls $DIRVMET/*.mvol 2>/dev/null | tail -n850); do
+		ARQ=$($BASENAME "$ARQFULL")
+		
+		# Ignora o arquivo se ainda estiver aberto por outro processo (em gravação)
+		if fuser -s "$ARQFULL"; then
+			continue
+		fi
+		
+		NUMVOL=$(strings "$ARQFULL" | grep scan[0-9] | wc -l)
+		echo "QTD ARQS: $NUMVOL"
+		
+		if [ "$NUMVOL" -eq 17 ]; then
+			INVALIDO=0
+			NOMEVOL=$($BASENAME "$ARQ" | sed -r 's/(.*)\--(.{2})\:(.{2})\:(.*\..*)/\1\_\2\3/')
+			ANO=$(echo "${NOMEVOL}" | cut -c1-4)
+			MES=$(echo "${NOMEVOL}" | cut -c6-7)
+			DIA=$(echo "${NOMEVOL}" | cut -c9-10)
+			
+			if [ -s "$DIRBASEVMET/$ANO/$MES/$DIA/${NOMEVOL}.mvol.bz2" ]; then
+				echo "ARQUIVO EXISTE $NOMEVOL"
+			else	
+				echo "ORGANIZANDO.. ARQUIVO VMET $ARQ"
+				mkdir -p "$DIRBASEVMET/$ANO/$MES/$DIA"
+				
+				echo "COPIANDO ARQUIVO $ARQ"
+				cp -rp "$ARQFULL" "$DIRBASEVMET/$ANO/$MES/$DIA/${NOMEVOL}.mvol"
+				mv "$ARQFULL" "$DIRVMET_VALIDOS/$ARQ"
+				$BZIP2 -f "$DIRBASEVMET/$ANO/$MES/$DIA/${NOMEVOL}.mvol"
+			fi
 		else
-			echo "ORGANIZANDO.. ARQUIVO XPPI "$ARQ
-			if [ ! -d $DIRBASEXPPI/$ANO ]; then mkdir $DIRBASEXPPI/$ANO; fi
-			if [ ! -d $DIRBASEXPPI/$ANO/$MES ]; then mkdir $DIRBASEXPPI/$ANO/$MES; fi
-			if [ ! -d $DIRBASEXPPI/$ANO/$MES/$DIA ]; then mkdir $DIRBASEXPPI/$ANO/$MES/$DIA; fi
-			echo "COPIANDO ARQUIVO "  $ARQ
-			$(cp -rp $DIRXPPI/$ARQ $DIRBASEXPPI/$ANO/$MES/$DIA/${NOMEVOL}_400.mvol)
-			mv $DIRXPPI/$ARQ $DIRXPPI/0movidos/$ARQ
-			bzip2 -f $DIRBASEXPPI/$ANO/$MES/$DIA/${NOMEVOL}_400.mvol
+			echo "$ARQ"
+			echo "Movendo arquivo VMET invalido ... ${NUMVOL}"
+			INVALIDO=1
+			mv $ARQFULL $DIRVMET_INVALIDOS/$ARQ
 		fi
-        fi
+	done
+	   
+	$RSYNC -avzr --no-t --remove-source-files --exclude-from=/home/muran/exclude.txt --files-from=<(ls $DIRBASEVMET/ 2>/dev/null | tail -n850) $DIRBASEVMET/ $DIRRSYNCVMET 
+
+	# SCAN XPPI
+	echo "CHECANDO SCAN XPPI"
+	for ARQFULL in $(ls $DIRXPPI/*.mvol 2>/dev/null | tail -n350); do
+		ARQ=$($BASENAME "$ARQFULL")
+		
+		# Ignora o arquivo se ainda estiver aberto por outro processo (em gravação)
+		if fuser -s "$ARQFULL"; then
+			continue
+		fi
+
+		NUMVOL=$(strings "$ARQFULL" | grep scan[0-9] | wc -l)
+		echo "$NUMVOL"
+
+		if [ "$NUMVOL" -eq 3 ]; then
+			NOMEVOL=$($BASENAME "$ARQ" | sed -r 's/(.*)\--(.{2})\:(.{2})\:(.*\..*)/\1\_\2\3/')
+			ANO=$(echo "${NOMEVOL}" | cut -c1-4)
+			MES=$(echo "${NOMEVOL}" | cut -c6-7)
+			DIA=$(echo "${NOMEVOL}" | cut -c9-10)
+
+			if [ -s "$DIRBASEXPPI/$ANO/$MES/$DIA/${NOMEVOL}_400.mvol.bz2" ]; then
+				echo "ARQUIVO EXISTE $NOMEVOL"
+				mv "$ARQFULL" "$DIRXPPI/0movidos/$ARQ"
+			else
+				echo "ORGANIZANDO.. ARQUIVO XPPI $ARQ"
+				mkdir -p "$DIRBASEXPPI/$ANO/$MES/$DIA"
+				
+				echo "COPIANDO ARQUIVO $ARQ"
+				cp -rp "$ARQFULL" "$DIRBASEXPPI/$ANO/$MES/$DIA/${NOMEVOL}_400.mvol"
+				mv "$ARQFULL" "$DIRXPPI_VALIDOS/$ARQ"
+				$BZIP2 -f "$DIRBASEXPPI/$ANO/$MES/$DIA/${NOMEVOL}_400.mvol"
+			fi
+		else
+			echo "movendo arquivo invalido ... ${NUMVOL}"
+			mv $ARQFULL $DIRXPPI_INVALIDOS/$ARQ
+
+		fi
+	done
+
+	$RSYNC -avzr --no-t --remove-source-files --exclude-from=/home/muran/exclude.txt --files-from=<(ls $DIRBASEXPPI/ 2>/dev/null | tail -n350) $DIRBASEXPPI/ $DIRRSYNCXPPI 
+
+	echo "invalido={$INVALIDO}"
+
+	# $ZABBIXSENDER -z 172.20.5.144 -s "srvcrmnradproc1p" -k status_num_elevacoes_$LOCAL_RADAR -o $INVALIDO
+
+	sleep 60 
 done
-
-rsync -avzr --no-t --remove-source-files --exclude-from=/home/muran/exclude.txt  --files-from=<(ls $DIRBASEXPPI/ | tail -n350) $DIRBASEXPPI/ $DIRRSYNCXPPI 
-
-echo "invalido={$INVALIDO}"
-$ZABBIXSENDER  -z 172.20.5.144 -s "srvcrmnradproc1p" -k status_num_elevacoes_$LOCAL_RADAR -o `echo $INVALIDO`
-
-# Remove arquivo de trava
-rm -f "${LOCKFILE}";
